96d6ce7ceecc17e63d0237d188b1bd96b81ab6f9
Brennan Payne <brennan.s.payne@gmail.com>
2017-01-09 14:50:14 -0600

    Add filters to dashboard

---
+++ app/assets/javascripts/_filters.js
@@ -0,0 +1,30 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var laneFilter = document.querySelector('#filterByLane');
+  var driverFilter = document.querySelector('#filterByDriver');
+  var customerFilter = document.querySelector('#filterByCustomer');
+  var truckFilter = document.querySelector('#filterByTruck');
+
+  var filters = [laneFilter, driverFilter, customerFilter, truckFilter];
+
+  filters.forEach(function(filter) {
+    filter.addEventListener('click', function(event) {
+      showFilterOptions(filters, filter);
+    });
+  });
+});
+
+function showFilterOptions(filters, filterDivToOpen) {
+  // console.warn(filterDivToOpen);
+  filters.forEach(function(filter) {
+    if(filterDivToOpen.id === filter.id) {
+      var filterOptions = document.querySelector('.' + filter.id + '__options')
+      filterOptions.classList.remove('is-hidden');
+    } else {
+      var filterOptions = document.querySelector('.' + filter.id + '__options')
+      filterOptions.classList.add('is-hidden');
+    }
+  });
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboard#index'
+  get 'lanes', to: 'dashboard#lanes'
+  get 'customers', to: 'dashboard#customers'
+  get 'drivers', to: 'dashboard#drivers'
 end
