6f4951f40fd544c270c3208f1d18a45e48814642
Brennan Payne <brennan.s.payne@gmail.com>
2017-01-11 20:43:26 -0600

    Routes & Some driver/trucks updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'lanes', to: 'dashboard#lanes'
   get 'customers', to: 'dashboard#customers'
   get 'drivers', to: 'dashboard#drivers'
+  get 'trucks', to: 'dashboard#trucks'
 end
