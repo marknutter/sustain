9d580e089c06172aa7a6b6e9ee6b5d939c8f8321
Brennan Payne <brennan.s.payne@gmail.com>
2017-01-12 07:39:35 -0600

    Check for driver emptyu string

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,9 @@ class DashboardController < ApplicationController
   def drivers
     @driver = Hash.new
     @driver[:name] = params[:driverName] || 'Driver Name'
+    if @driver[:name].empty?
+      @driver[:name] = 'Driver Name'
+    end
     @driver[:sustainability_score] = '89.3'
   end
 end
